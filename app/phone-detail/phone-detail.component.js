fa00d7ed3ef2d1dd08573a5e047fe7d5bd578d03
Craig <shangpeizheng@gmail.com>
2020-08-06 11:22:34 +1000

    Update phone-detail.component.js

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -1,16 +1,17 @@
-angular.module('phoneDetail').component('phoneDetail', {
-    templateUrl: 'phone-detail/phone-detail.template.html',
-    controller: ['$routeParams', '$http',
-        function PhoneDetailController($routeParams, $http) {
-            var self = this;
+angular
+    .module('phoneDetail')
+    .component('phoneDetail', {
+        templateUrl: 'phone-detail/phone-detail.template.html',
+        controller: ['$routeParams', 'Phone',
+            function PhoneDetailController($routeParams, Phone) {
+                var self = this;
 
-            self.setImage = function setImage(imageUrl) {
-                self.mainImageUrl = imageUrl;
-            }
+                self.phone = Phone.get({phoneId: $routeParams.phoneId}, function (phone) {
+                    self.setImage(phone.images[0]);
+                })
 
-            $http.get('phones/' + $routeParams.phoneId + '.json').then(function (response) {
-                self.phone = response.data;
-                self.setImage(self.phone.images[0]);
-            })
-        }]
-})
+                self.setImage = function setImage(imageUrl) {
+                    self.mainImageUrl = imageUrl;
+                }
+            }]
+    })
