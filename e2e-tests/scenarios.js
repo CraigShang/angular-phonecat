e060aee02b2d7570ecc6555d4b76b445f2b014b6
Craig <shangpeizheng@gmail.com>
2020-08-07 11:02:31 +1000

    Update scenarios.js

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,87 +1,95 @@
 'use strict';
+describe('PhoneCat Application', function() {
 
-// AngularJS E2E Testing Guide:
-// https://docs.angularjs.org/guide/e2e-testing
+  it('should redirect `index.html` to `index.html#!/phones', function() {
+    browser.get('index.html');
+    expect(browser.getCurrentUrl()).toContain('index.html#!/phones');
+  });
 
-describe('PhoneCat Application', function () {
+  describe('View: Phone list', function() {
 
-    it('should redirect `index.html` to `index.html#!/phones`', function () {
-        browser.get('index.html');
-        expect(browser.getCurrentUrl()).toContain('index.html#!/phones');
+    beforeEach(function() {
+      browser.get('index.html#!/phones');
     });
 
-    describe("View: phoneList", function () {
+    it('should filter the phone list as a user types into the search box', function() {
+      var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
+      var query = element(by.model('$ctrl.query'));
 
-        beforeEach(function () {
-            browser.get('index.html#!/phones');
-        });
+      expect(phoneList.count()).toBe(20);
 
-        it("should filter the phone list as a user types into the search box", function () {
-            var phoneList = element.all(by.repeater("phone in $ctrl.phones"));
-            var query = element(by.model('$ctrl.query'));
-            expect(phoneList.count()).toBe(3);
-            query.sendKeys('nexus');
-            expect(phoneList.count()).toBe(3);
-            query.clear();
-            query.sendKeys('motorola');
-            expect(phoneList.count()).toBe(2);
-        });
+      query.sendKeys('nexus');
+      expect(phoneList.count()).toBe(1);
 
-        it("should be possible to control phone order via the drop-down menu", function () {
-            var queryField = element(by.model("$ctrl.query"));
-            var orderSelect = element(by.model("$ctrl.orderProp"));
-            var nameOption = orderSelect.element(by.css('option[value="name"]'));
-            var phoneNameColumn = element.all(by.repeater("phone in $ctrl.phones").column("phone.name"));
-
-            function getNames() {
-                return phoneNameColumn.map(function (elem) {
-                    return elem.getText();
-                });
-            }
-
-            queryField.sendKeys("tablet");
-
-            expect(getNames()).toEqual([
-                'Motorola XOOM\u2122 with Wi-Fi',
-                'MOTOROLA XOOM\u2122'
-            ]);
-
-            nameOption.click();
-
-            expect(getNames()).toEqual([
-                'MOTOROLA XOOM\u2122',
-                'Motorola XOOM\u2122 with Wi-Fi'
-            ]);
-        });
-
-        it("should render phonespecific links", function () {
-            var query = element(by.model("$ctrl.query"));
-            query.sendKeys("nexus");
-            element.all(by.css(".phones li a")).first().click();
-            expect(browser.getCurrentUrl()).toContain("index.html#!/phones/nexus-s")
-        });
+      query.clear();
+      query.sendKeys('motorola');
+      expect(phoneList.count()).toBe(8);
     });
 
-    describe("View: Phone details", function () {
-        beforeEach(function () {
-            browser.get("index.html#!/phones/nexus-s");
-        });
+    it('should be possible to control phone order via the drop-down menu', function() {
+      var queryField = element(by.model('$ctrl.query'));
+      var orderSelect = element(by.model('$ctrl.orderProp'));
+      var nameOption = orderSelect.element(by.css('option[value="name"]'));
+      var phoneNameColumn = element.all(by.repeater('phone in $ctrl.phones').column('phone.name'));
 
-        it('should display the `nexus-s` page', function () {
-            expect(element(by.binding('$ctrl.phone.name')).getText()).toBe('Nexus S');
+      function getNames() {
+        return phoneNameColumn.map(function(elem) {
+          return elem.getText();
         });
+      }
 
-        it('should display the first phone image as the main phone image', function () {
-            var mainImage = element(by.css('img.phone'));
-            expect(mainImage.getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
-        });
-        it('should swap the main image when clicking on a thumbnail image', function () {
-            var mainImage = element(by.css('img.phone'));
-            var thumbnails = element.all(by.css('.phone-thumb img'));
-            thumbnails.get(2).hover();
-            expect(mainImage.getAttribute('src')).toMatch(/img\/phones\/nexus-s.2.jpg/);
-            thumbnails.get(0).hover();
-            expect(mainImage.getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
-        });
-    })
+      queryField.sendKeys('tablet');   // Let's narrow the dataset to make the assertions shorter
+
+      expect(getNames()).toEqual([
+        'Motorola XOOM\u2122 with Wi-Fi',
+        'MOTOROLA XOOM\u2122'
+      ]);
+
+      nameOption.click();
+
+      expect(getNames()).toEqual([
+        'MOTOROLA XOOM\u2122',
+        'Motorola XOOM\u2122 with Wi-Fi'
+      ]);
+    });
+
+    it('should render phone specific links', function() {
+      var query = element(by.model('$ctrl.query'));
+      query.sendKeys('nexus');
+
+      element.all(by.css('.phones li a')).first().click();
+      expect(browser.getCurrentUrl()).toContain('index.html#!/phones/nexus-s');
+    });
+
+  });
+
+  describe('View: Phone detail', function() {
+
+    beforeEach(function() {
+      browser.get('index.html#!/phones/nexus-s');
+    });
+
+    it('should display the `nexus-s` page', function() {
+      expect(element(by.binding('$ctrl.phone.name')).getText()).toBe('Nexus S');
+    });
+
+    it('should display the first phone image as the main phone image', function() {
+      var mainImage = element(by.css('img.phone.selected'));
+
+      expect(mainImage.getAttribute('src')).toContain('img/phones/nexus-s.0.jpg');
+    });
+
+    it('should swap the main image when clicking on a thumbnail image', function() {
+      var mainImage = element(by.css('img.phone.selected'));
+      var thumbnails = element.all(by.css('.phone-thumbs img'));
+
+      thumbnails.get(2).click();
+      expect(mainImage.getAttribute('src')).toContain('img/phones/nexus-s.2.jpg');
+
+      thumbnails.get(0).click();
+      expect(mainImage.getAttribute('src')).toContain('img/phones/nexus-s.0.jpg');
+    });
+
+  });
+
 });
